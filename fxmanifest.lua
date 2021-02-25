a7b4954e33773a828e4dd17e6aa89456b5d32664
thelindat <65407488+thelindat@users.noreply.github.com>
2021-02-26 03:17:14 +1100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 games { 'gta5' }
 
-version '1.9.3'
+version '1.9.4'
 description 'https://github.com/thelindat/nui_doorlock'
 versioncheck 'https://raw.githubusercontent.com/thelindat/nui_doorlock/main/fxmanifest.lua'
 
